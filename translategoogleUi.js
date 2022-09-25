95337d214052fc54a371b4d4dec224dd716cef70
Ui Kit Ux <114171496+uikitux@users.noreply.github.com>
2022-09-26 00:25:59 +0700

    Create translategoogleUi.js

---
+++ translategoogleUi.js
@@ -0,0 +1,1 @@
+function googleTranslateElementInit(){new google.translate.TranslateElement({pageLanguage:&#39;id&#39;,includedLanguages:&#39;en,id&#39;,layout:google.translate.TranslateElement.InlineLayout.SIMPLE},&#39;google_translate_element&#39;)}$(&quot;.hover&quot;).mouseleave(function (){$(this).removeClass(&quot;hover&quot;)});
